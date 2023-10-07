5017b5778c0157ae032080d97b5c7902e95fd1ed
Jialin-Shao <147238386+Jialin-Shao@users.noreply.github.com>
2023-10-07 19:28:29 +0800

    Add files via upload

---
+++ Sequence_analysis/Sequence_analysis.sh
@@ -0,0 +1,33 @@
+echo "Program starts"
+
+#Prepare sequences without primers from raw data. All parameters you may need to adjust are in this block.
+./flash2 Sample_1.fq.gz Sample_2.fq.gz -p 33 -r 100 -f 140 -s 100 -o Sample.fq #If you do not have compatibility issues for flash2 and do not have an executable file in the folder, use "/flash2" instead of "./flash2" 
+cutadapt -a 'TCCCACCTACCTACAGAGCT' --discard-untrimmed -m 21 -o Sample.fq.extendedFrags_trimend.fastq Sample.fq.extendedFrags.fastq
+cutadapt -g 'TTCGGTGTTCAGGTCCTGGC' --discard-untrimmed -m 21 -o Sample.fq.extendedFrags_trimend.fast_aptamer.fastq Sample.fq.extendedFrags_trimend.fastq
+
+#Match the q30 sequences with the given pool
+bwa index pool.fasta
+echo "Start matching..."
+bwa mem pool.fasta Sample.fq.extendedFrags_trimend.fast_aptamer.fastq | samtools view -bSh -q 30 -@ 10 -o Sample_q30_addprimers.bam -
+echo "Start sorting..."
+samtools sort Sample_q30_addprimers.bam -@ 8 -o Sample_q30.sorted_addprimers.bam
+samtools index Sample_q30.sorted_addprimers.bam
+samtools view Sample_q30.sorted_addprimers.bam >Sample_q30.sorted_addprimers.sam
+awk '{if (/\^/) {print $0}}' Sample_q30.sorted_addprimers.sam>Sample_delet.txt
+awk '{if (/SA:Z/) {print $0}}' Sample_q30.sorted_addprimers.sam >Sample_insert.txt
+awk '{if(/NM:i:0/) {} else {print $0}}' Sample_q30.sorted_addprimers.sam | awk '{if (/SA:Z/) {} else {print $0}}' | awk '{if (/\^/) {} else {print $0}}'>Sample_snp.txt
+awk '{if (/NM:i:0/) {print $0}}' Sample_q30.sorted_addprimers.sam >Sample_perfect_match.txt
+
+#Generate statistics
+awk '{print $1"\t"$3"\t"$10}' Sample_perfect_match.txt > Sample_perfect_match_chuli.txt
+awk '{print $2}' Sample_perfect_match_chuli.txt | uniq -c >Sample_perfect_match_statistics.txt
+
+#Prepare file for decoding
+echo "Start counting reads..."
+wc -l Sample.fq.extendedFrags_trimend.fast_aptamer.fastq | awk '{print $1/4}' > total_reads_count.txt
+echo "start preparing decoding file. This will take a while..."
+seqtk trimfq -q 0.05 -l 21 Sample.fq.extendedFrags_trimend.fast_aptamer.fastq > Sample.trimmed.fastq
+awk 'NR%4==2' Sample.trimmed.fastq | sort | uniq -c | sort -nr > Sample_sequence_counts.txt
+awk '{print $2}' Sample_sequence_counts.txt > Sample_sequence_sorted.txt
+
+echo "Program ends"
